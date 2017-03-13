8dd020ebd3432c088d3b00f1d352cbe9f65ac971
kayoung <kayounglee@MacBook-Pro.local>
2017-03-12 21:58:33 -0400

    Code to 1) convert the data type of two columns 'start.time' and 'stop.time' from factor to date 2) Merge them in one data frame 3) Extract data frame as CSV

---
+++ data_merge_convert.R
@@ -0,0 +1,24 @@
+# Read the data
+
+oct16 <- read.csv("201610-citibike-tripdata.csv")
+nov16 <- read.csv("201611-citibike-tripdata.csv")
+dec16 <- read.csv("201612-citibike-tripdata.csv")
+
+
+# Convert the data type of column 'starttime' from factor to date
+
+oct16$Start.Time <- strptime(oct16$Start.Time, format = "%Y-%m-%d %H:%M:%S")
+oct16$Stop.Time <- strptime(oct16$Stop.Time, format = "%Y-%m-%d %H:%M:%S")
+nov16$Start.Time <- strptime(nov16$Start.Time, format = "%Y-%m-%d %H:%M:%S")
+nov16$Stop.Time <- strptime(nov16$Stop.Time, format = "%Y-%m-%d %H:%M:%S")
+dec16$Start.Time <- strptime(dec16$Start.Time, "%Y-%m-%d %H:%M:%S")
+dec16$Stop.Time <- strptime(dec16$Stop.Time, format = "%Y-%m-%d %H:%M:%S")
+
+
+# Merge the data of three months
+fourq2016 <- rbind(oct16, nov16, dec16)
+
+
+# Extract as CSV
+write.csv(fourq2016, file="fourq2016.csv")
+write.csv(dec16, file="dec16.csv")
